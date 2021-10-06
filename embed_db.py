8aaddbcbf48e310222dafcc8f94d1ab1bc471763
tzom <tomolus@gmail.com>
2021-10-06 13:34:57 +0200

    speedup peptide embedding plaint python: map and .ljust :)

--- embed_db.py
+++ embed_db.py
@@ -1,50 +1,53 @@
 import sys 
-sys.path.append("../dnovo3")
-from tf_data_json import parse_peptide
-from load_model import spectrum_embedder,sequence_embedder
 import glob, os
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-import json
+import argparse
+parser = argparse.ArgumentParser(description='convert')
+parser.add_argument('--DB_DIR', default='./DB', type=str, help='path to db file')
+parser.add_argument('--GPU', default='-1', type=str, help='GPU id')
+args = parser.parse_args()
+GPU = args.GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = GPU
+sys.path.append("../dnovo3")
+
+from preprocessing import aa_with_pad, MAX_PEPTIDE_LENGTH
+from load_model import spectrum_embedder,sequence_embedder
+
 from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
-import multiprocessing
 
-AUTOTUNE=tf.data.AUTOTUNE
+def get_sequence_of_indices(sequence: list, aa_list: list=list(aa_with_pad)):
+    indices = [aa_list.index(aa) for aa in sequence]
+    return indices
 
-import argparse
+def trim_sequence(sequence:str,MAX_PEPTIDE_LENGTH=MAX_PEPTIDE_LENGTH):
+    sequence = sequence.ljust(MAX_PEPTIDE_LENGTH,'_')
+    return list(sequence)
 
-parser = argparse.ArgumentParser(description='convert')
-parser.add_argument('--DB_DIR', default='./DB', type=str, help='path to db file')
-
-args = parser.parse_args()
+AUTOTUNE=tf.data.experimental.AUTOTUNE
 
 DB_DIR=args.DB_DIR
-BATCH_SIZE=4096
-
-#DB_DIR = './db'
-#DB_DIR = './db_miscleav_1'
+BATCH_SIZE=4*4096
 
 def parse_peptide_(peptide):
-    try:
-        return parse_peptide(peptide)
-    except:
-        print(peptide)
-
-
+    peptide = trim_sequence(peptide)
+    peptide = get_sequence_of_indices(peptide)
+    return peptide
 
 if __name__ == '__main__':    
-    peptides = np.load(os.path.join(DB_DIR,"peptides.npy"))
-    with multiprocessing.Pool(64) as p:
-        peptides = list(p.imap(parse_peptide,tqdm(peptides)))
+    peptides = np.load(os.path.join(DB_DIR,"peptides.npy"))#[:1000000]
+
+    peptides = list(map(trim_sequence,tqdm(peptides)))
+    peptides = list(map(get_sequence_of_indices,tqdm(peptides)))
+    
+    peptides = np.array(peptides,dtype=np.int32)
+    print(peptides.shape)
 
     def get_dataset(peptides,batch_size=BATCH_SIZE):
-        output_dtype = [tf.int32]
-        ds = tf.data.Dataset.from_tensor_slices(peptides)
-        #ds = ds.map(lambda x: tf.numpy_function(lambda x: parse_peptide(x), [x], output_dtype),
-        #ds = ds.map(lambda x: tf.numpy_function(lambda x: parse_peptide(str(x)[2:-1]), [x], output_dtype),
-        #            num_parallel_calls=AUTOTUNE, deterministic=False)
-        ds = ds.batch(batch_size)
+        def peptide_generator():
+            for i in range(0, len(peptides), batch_size):
+                yield peptides[i:i + batch_size]
+        ds = tf.data.Dataset.from_generator(peptide_generator,tf.int32)
         ds = ds.prefetch(AUTOTUNE)
         return ds
 

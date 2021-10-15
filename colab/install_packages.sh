f9a93e3ad3644581bb4b4deae565f03cd6a53388
tzom <tomolus@gmail.com>
2021-10-15 09:14:59 +0000

    add colab example scripts

---
+++ colab/install_packages.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+apt install libomp-dev
+pip install faiss-gpu
+pip install --upgrade tables
---
+++ colab/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OUTPUT_DIR='data/forward'
+DB_DIR=$OUTPUT_DIR'/db_miscleav_1'
+
+DECOY_OUTPUT_DIR='data/rev'
+DECOY_DB_DIR=$DECOY_OUTPUT_DIR'/db_miscleav_1'
+
+FASTA='data/559O92.fasta.gz'
+MGF='data/Hsp31 peak list 1-1.mgf'
+
+GPU='0'
+
+mkdir -p $OUTPUT_DIR
+mkdir -p $DB_DIR
+
+mkdir -p $DECOY_OUTPUT_DIR
+mkdir -p $DECOY_DB_DIR
+
+python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DB_DIR
+python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DECOY_DB_DIR --REVERSE_DECOY=TRUE
+
+python embed_db.py --DB_DIR=$DB_DIR --GPU=$GPU
+python embed_db.py --DB_DIR=$DECOY_DB_DIR --GPU=$GPU
+
+python search.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR --MGF="$MGF" --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
+
+python search_score.py --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
+python fdr_filter.py --OUTPUT_DIR=$OUTPUT_DIR 

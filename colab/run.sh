994713af72b4e98427569c086235af122936e0cc
tzom <tomolus@gmail.com>
2021-10-15 09:59:54 +0000

    minor

--- colab/run.sh
+++ colab/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+mkdir -p "./data"
+cp /content/drive/MyDrive/datasets/PXD026566/* ./data
+
 OUTPUT_DIR='data/forward'
 DB_DIR=$OUTPUT_DIR'/db_miscleav_1'
 
 DECOY_OUTPUT_DIR='data/rev'
 DECOY_DB_DIR=$DECOY_OUTPUT_DIR'/db_miscleav_1'
 
-FASTA='data/559O92.fasta.gz'
-MGF='data/Hsp31 peak list 1-1.mgf'
+FASTA='data/10090.fasta.gz'
+MGF='data/20210311_HW4_001.mgf'
 
 GPU='0'
 
@@ -17,13 +20,13 @@ mkdir -p $DB_DIR
 mkdir -p $DECOY_OUTPUT_DIR
 mkdir -p $DECOY_DB_DIR
 
-python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DB_DIR
-python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DECOY_DB_DIR --REVERSE_DECOY=TRUE
+python ../fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DB_DIR
+python ../fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DECOY_DB_DIR --REVERSE_DECOY=TRUE
 
-python embed_db.py --DB_DIR=$DB_DIR --GPU=$GPU
-python embed_db.py --DB_DIR=$DECOY_DB_DIR --GPU=$GPU
+# python ../embed_db.py --DB_DIR=$DB_DIR --GPU=$GPU
+# python ../embed_db.py --DB_DIR=$DECOY_DB_DIR --GPU=$GPU
 
-python search.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR --MGF="$MGF" --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
+# python ../search.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR --MGF="$MGF" --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
 
-python search_score.py --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
-python fdr_filter.py --OUTPUT_DIR=$OUTPUT_DIR 
+# python ../search_score.py --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
+# python ../fdr_filter.py --OUTPUT_DIR=$OUTPUT_DIR 

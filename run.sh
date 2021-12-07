41d090d045cef8a9e90557aef86133246f683465
tzom <tomolus@gmail.com>
2021-12-07 17:08:58 +0100

    minor

--- run.sh
+++ run.sh
@@ -9,10 +9,10 @@ conda activate yhydra_gpu_env
 RESULTS_DIR=$(cat config.yaml | yq -r .RESULTS_DIR)
 
 OUTPUT_DIR=$RESULTS_DIR'/forward'
-DB_DIR=$OUTPUT_DIR'/db_miscleav_1'
+DB_DIR=$OUTPUT_DIR'/db'
 
 DECOY_OUTPUT_DIR=$RESULTS_DIR'/rev'
-DECOY_DB_DIR=$DECOY_OUTPUT_DIR'/db_miscleav_1'
+DECOY_DB_DIR=$DECOY_OUTPUT_DIR'/db'
 
 FASTA=$(cat config.yaml | yq -r .FASTA)
 MGFs=$(cat config.yaml | yq -r .MGFs)
@@ -20,12 +20,13 @@ MGFs=$(cat config.yaml | yq -r .MGFs)
 MGFs=$(ls ${MGFs})
 
 GPU=$(cat config.yaml | yq .GPU)
+MAX_MISSED_CLEAVAGES=$(cat config.yaml | yq .MAX_MISSED_CLEAVAGES)
 
 mkdir -p $OUTPUT_DIR
 mkdir -p $DECOY_OUTPUT_DIR
 
-python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DB_DIR
-python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=1 --DB_DIR=$DECOY_DB_DIR --REVERSE_DECOY=TRUE
+python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=$MAX_MISSED_CLEAVAGES --DB_DIR=$DB_DIR
+python fasta2db.py --FASTA_FILE=$FASTA --MAX_MISSED_CLEAVAGES=$MAX_MISSED_CLEAVAGES --DB_DIR=$DECOY_DB_DIR --REVERSE_DECOY=TRUE
 python sanitize_db.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR
 
 python embed_db.py --DB_DIR=$DB_DIR --GPU=$GPU

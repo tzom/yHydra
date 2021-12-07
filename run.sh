80352ca83ec1ffd49aaa9f7974e5cc7acc9e32eb
tzom <tomolus@gmail.com>
2021-12-07 20:48:03 +0100

    add example

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+CONFIG="$1"
 #source /hpi/fs00/home/tom.altenburg/conda/bin/activate 
 #conda activate yhydra_env
 
 source /hpi/fs00/home/tom.altenburg/conda/bin/activate 
 conda activate yhydra_gpu_env
 
-RESULTS_DIR=$(cat config.yaml | yq -r .RESULTS_DIR)
+RESULTS_DIR=$(cat $CONFIG | yq -r .RESULTS_DIR)
 
 OUTPUT_DIR=$RESULTS_DIR'/forward'
 DB_DIR=$OUTPUT_DIR'/db'
@@ -14,13 +14,17 @@ DB_DIR=$OUTPUT_DIR'/db'
 DECOY_OUTPUT_DIR=$RESULTS_DIR'/rev'
 DECOY_DB_DIR=$DECOY_OUTPUT_DIR'/db'
 
-FASTA=$(cat config.yaml | yq -r .FASTA)
-MGFs=$(cat config.yaml | yq -r .MGFs)
+FASTA=$(cat $CONFIG | yq -r .FASTA)
+FASTA=$(ls ${FASTA})
 
-MGFs=$(ls ${MGFs})
+#MGFs=$(cat $CONFIG | yq -r .MGFs)
+#MGFs=$(ls ${MGFs})
 
-GPU=$(cat config.yaml | yq .GPU)
-MAX_MISSED_CLEAVAGES=$(cat config.yaml | yq .MAX_MISSED_CLEAVAGES)
+RAWs=$(cat $CONFIG | yq -r .RAWs)
+RAWs=$(ls ${RAWs})
+
+GPU=$(cat $CONFIG | yq .GPU)
+MAX_MISSED_CLEAVAGES=$(cat $CONFIG | yq .MAX_MISSED_CLEAVAGES)
 
 mkdir -p $OUTPUT_DIR
 mkdir -p $DECOY_OUTPUT_DIR
@@ -32,8 +36,16 @@ python sanitize_db.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR
 python embed_db.py --DB_DIR=$DB_DIR --GPU=$GPU
 python embed_db.py --DB_DIR=$DECOY_DB_DIR --GPU=$GPU
 
-for MGF in $MGFs
+for RAW in $RAWs
 do
+   mono ext/ThermoRawFileParser/ThermoRawFileParser.exe -L=2 -f=0 -i=$RAW &
+done
+
+wait
+
+for RAW in $RAWs
+do
+   MGF=${RAW%.raw}.mgf
    #mono ~/tools/ThermoRawFileParser/ThermoRawFileParser.exe -L=2 -f=0 -s -i=$MGF | python search.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR --MGF=$MGF --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
    python search.py --DB_DIR=$DB_DIR --DECOY_DB_DIR=$DECOY_DB_DIR --MGF=$MGF --OUTPUT_DIR=$OUTPUT_DIR --GPU=$GPU
 done

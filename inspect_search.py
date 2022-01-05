95d6689b01e4683e39a9d3df6e147f90102f1d48
tzom <tomolus@gmail.com>
2022-01-05 11:56:42 +0100

    minor

--- inspect_search.py
+++ inspect_search.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 
-with pd.HDFStore(os.path.join('%s/forward/'%(RESULTS_DIR),'search_results_scored_filtered.h5')) as store:
+with pd.HDFStore(os.path.join('%s/'%(RESULTS_DIR),'search_results_scored_filtered.h5')) as store:
     raw_files = store.keys()
     search_results = pd.concat([store[key] for key in raw_files],ignore_index=True)
 

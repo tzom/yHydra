022280e520a3e3caf3f0957bc3e4a5c75270bd4a
tzom <tomolus@gmail.com>
2022-01-13 12:08:48 +0100

    minor

--- run.py
+++ run.py
@@ -12,9 +12,6 @@ from fasta2db import digest_fasta
 f = digest_fasta(FASTA,REVERSE_DECOY=False)
 r = digest_fasta(FASTA,REVERSE_DECOY=True)
 
-from pyThermoRawFileParser import parse_rawfiles
-raw = parse_rawfiles(RAWs)
-
 from sanitize_db import sanitize_db
 s = sanitize_db()
 
@@ -22,8 +19,12 @@ from embed_db import embed_db
 e = embed_db(REVERSE_DECOY=False)
 e = embed_db(REVERSE_DECOY=True)
 
+from pyThermoRawFileParser import parse_rawfiles
+raw = parse_rawfiles(RAWs)
+
 from search import search
 s = [search(RAW.replace('.raw','.mgf')) for RAW in RAWs]
+#s = [search(RAW) for RAW in RAWs]
 
 from search_score import search_score
 search_score()

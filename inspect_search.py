87a8425e94d631b9d2c647a4d0760dc12abd4887
ani <sharma.animesh@gmail.com>
2022-03-29 18:52:21 +0200

    change config to run over 220317_Alessandro_test2_sample3x-replicate2-5ul-injection4  and write search_results to csv

--- inspect_search.py
+++ inspect_search.py
@@ -1,9 +1,18 @@
+import sys,os 
+os.environ['YHYDRA_CONFIG'] = sys.argv[1]
+from load_config import CONFIG
+
+RESULTS_DIR = CONFIG['RESULTS_DIR']
+
+#%%
 import pandas as pd
 import os
 
 
-with pd.HDFStore(os.path.join('example/search/forward/','search_results_scored_filtered.h5')) as store:
+with pd.HDFStore(os.path.join('%s/'%(RESULTS_DIR),'search_results_scored_filtered.h5')) as store:
     raw_files = store.keys()
     search_results = pd.concat([store[key] for key in raw_files],ignore_index=True)
 
-print(search_results)
+selected_columns = ['raw_file','scan','index','precursorMZ','pepmass','charge','best_is_decoy','best_distance','best_score','best_peptide','peptide_mass','delta_mass','accession']
+search_results[selected_columns].to_csv("search_results.csv")
+print(search_results[selected_columns])
